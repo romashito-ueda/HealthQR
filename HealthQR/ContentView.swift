a1c06e10bb2ae3722a2d6430b9c799e55e942b7b
Toshimaro <98173327+Toshimaro@users.noreply.github.com>
2025-06-17 19:46:54 +0900

    Initial Commit

---
+++ HealthQR/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HealthQR
+//
+//  Created by r0n on 2025/06/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HealthQR/HealthQRApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthQRApp.swift
+//  HealthQR
+//
+//  Created by r0n on 2025/06/17.
+//
+
+import SwiftUI
+
+@main
+struct HealthQRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
